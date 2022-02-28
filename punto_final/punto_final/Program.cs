6f6d04e5b436e0b5766807cbe7b5b9718a75c483
JesusRestrepo <jesusrestrepo2003@gmail.com>
2022-02-28 14:37:38 -0500

    ultimo punto

---
+++ punto_final/punto_final/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace punto_10
+{
+    class Program
+    {
+  
+        static void Main(string[] args)
+        {
+            int suma = 0, num = 1;
+            Console.WriteLine("Enter a number");
+            int n = int.Parse(Console.ReadLine());
+            while (num <= n)
+            {
+                suma = suma + num;
+                num = num + 1;
+            }
+
+            Console.WriteLine("You entered "+suma);
+        }
+    }
+}
